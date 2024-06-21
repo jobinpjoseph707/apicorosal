e26783d6133570fc6be4db3b7cfb2b6bb8079037
jobinpjoseph707 <jobinpjoseph700@gmail.com>
2024-06-21 10:36:08 +0530

    api fatch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'frontend';
+// [x: string]: any;
+  title = 'Jobin';
+  // email:string="";
+// productsData: any;
+// products: any;
+productNames: any;
+catagorys:string[]=[];
+  async getData(){
+    const products = await fetch('https://dummyjson.com/products')
+    const productsData = await products.json();
+    this.productNames = productsData.products;
+    console.log(this.productNames);
+    console.log(this.productNames[0].category);
+    this.productNames.map((Names:any)=>{
+      if(!this.catagorys.includes(Names.category)){
+        this.catagorys.push(Names.category)
+        // console.log(this.catagorys);
+        
+
+      }
+
+    })
+   
+  }
+  ngOnInit(){
+    this.getData();
+  }
 }
+
